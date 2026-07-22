14e7f290f23719e3364e56499b2bfc5ef3d46a9a
Yosuke Arimoto <yosuke@arimoto.biz>
2026-07-22 10:29:18 +0900

    gridは保存しない

--- scripts/on_image_saved.py
+++ scripts/on_image_saved.py
@@ -142,10 +142,9 @@ def on_image_saved(params:script_callbacks.ImageSaveParams) -> None:
         tags = []
         if shared.opts.save_generationinfo_to_immich_as_annotation:
             annotation = info
-        if params.filename.find('grid-') >= 0:
-            tags.append('グリッド')
-        else:
-            tags += pickuptags(pos_prompt)
+        if params.filename.find('grid-') == 0:
+            return
+        tags += pickuptags(pos_prompt)
         info = params.pnginfo.get('parameters', None)
         if shared.opts.save_positive_prompt_to_immich_as_tags:
             if len(pos_prompt.split(",")) > 0:
